f2c03d58b6b471e375f3e1e884a895bb2305576a
João Freitas <1160907@isep.ipp.pt>
2025-11-11 19:16:55 +0000

    [charset] - add blocks for party count, original trainers and names

--- block/block.go
+++ block/block.go
@@ -10,3 +10,14 @@ type Block struct {
 func (b Block) Read(data []byte) []byte {
 	return data[b.Offset : b.Offset+b.Size]
 }
+
+// ReadMultiple reads the data of [count] sequentially divided blocks.
+func (b Block) ReadMultiple(data []byte, count int) [][]byte {
+	d := make([][]byte, count)
+	for i := range count {
+		off := b.Offset + (b.Size * uint16(i))
+		d[i] = data[off : off+b.Size]
+	}
+
+	return d
+}
