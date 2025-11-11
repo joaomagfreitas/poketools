263d58a3e933174759e1e757c7d428c57a3ec23b
João Freitas <1160907@isep.ipp.pt>
2025-11-11 19:20:01 +0000

    [charset] - add party data to trainer record

--- trainer.go
+++ trainer.go
@@ -2,7 +2,16 @@ package poketools
 
 // Trainer records data about the player.
 type Trainer struct {
-	Name  string
-	Money uint32
-	Id    uint16
+	Name      string
+	Money     uint32
+	Id        uint16
+	RivalName string
+	Party     Party
+}
+
+// Party records data about the player party pokemon.
+type Party struct {
+	Count uint8
+	OT    []string
+	Names []string
 }
